545c546d9d986f6f95a49c28ec7541f3c0579df3
gersonbc79 <gersonbc_33@hotmail.com>
2022-04-05 14:23:33 -0500

    Se modifico la funcion de identificación de usuario solo para ser solucitada cuando se elimine un coemtario o un articulo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
-    http_basic_authenticate_with name: "dhh", password: "secret", 
-    except: [:index, :show]
     
+    http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+    
+
     def index
         @articles = Article.all
     end
